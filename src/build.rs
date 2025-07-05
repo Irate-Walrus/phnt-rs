766a4ec7d97f83fd06d6b3099409beca81986cb0
Irate-Walrus <71628949+Irate-Walrus@users.noreply.github.com>
2025-07-05 13:13:39 +1000

    fix: i probably should use rust analyzer

--- src/build.rs
+++ src/build.rs
@@ -174,7 +174,7 @@ mod regen {
 
       // this is a hack and will break stuff
       src = src.replace("extern \"stdcall\"", "extern \"system\"")
-      src = src.replace("extern \"fastcall\"", "extern \"system\"")
+      src.replace("extern \"fastcall\"", "extern \"system\"")
    }
 
    pub fn main() {
